a8af6b0130d1aa836a5d1a1a76b388ffd6999883
Guy Reading <guyreading@live.co.uk>
2026-09-12 11:40:28 +0100

    hotfix: create new game line spacing

--- container_rl/client/tui.py
+++ container_rl/client/tui.py
@@ -1307,9 +1307,9 @@ def _create_screen():
 
         body = f"[bold]Create New Game[/bold]\n[dim]Playing as: {MY_NAME}[/dim]\n\n"
         body += f"  [dim]{left_p}[/dim]  [bold yellow]{''.join(bar_p)}[/bold yellow] {num_players}  [dim]{right_p}[/dim]{sel_p}\n"
-        body += f"  [dim]Players[/dim]\n"
+        body += f"  [dim]Players[/dim]\n\n"
         body += f"  [dim]{left_a}[/dim]  [bold yellow]{''.join(bar_a)}[/bold yellow] {ai_players}  [dim]{right_a}[/dim]{sel_a}\n"
-        body += f"  [dim]AI opponents[/dim]\n"
+        body += f"  [dim]AI opponents[/dim]\n\n"
         body += f"  [dim]{left_c}[/dim]  [bold yellow]{''.join(bar_c)}[/bold yellow] {containers}  [dim]{right_c}[/dim]{sel_c}\n"
         body += f"  [dim]Containers per colour ({note_c})[/dim]\n\n"
         if ai_players > 0:
